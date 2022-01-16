fe87ca2d21e6bd2719d1bed42807c2a22fef1800
r8now <51028108+r8now@users.noreply.github.com>
2022-01-16 19:28:33 +0100

    kommentarer

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const FriendModel = require("./models/Friends");
 require('dotenv').config();
-
+//cors används så att min front och back end får en anslutning.
 app.use(cors());
 app.use(express.json());
 
 // database connection
-                       
-mongoose.connect("mongodb+srv://hosein:sultan.1@mern.dplbs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser: true,
+mongoose.connect("mongodb+srv://hosein:sultan.1@mern.dplbs.mongodb.net/myFirstDatabase?////retryWrites=true&w=majority", {useNewUrlParser: true,
 useUnifiedTopology: true});
 
 
 
+//lägga till en ny vän med namn och ålder
 app.post("/addfriend", async (req, res) => {
     const name = req.body.name;
     const age = req.body.age;
@@ -24,6 +24,7 @@ await friend.save();
 res.send(friend);
 });
 
+//Hämtar min data, och finns det något fel så visas den mha min error annarsn visas resultatet,
 app.get("/read", async (req, res) => {
    FriendModel.find({}, (err, result) => {
     if (err) {
@@ -36,7 +37,7 @@ app.get("/read", async (req, res) => {
    
     });
 
-
+//uppdaterar åldern från listan 
 app.put('/update', async(req, res, next) => {
 const newAge = req.body.newAge;
 const id = req.body.id;
@@ -51,12 +52,12 @@ friendToUpdate.save();
 }
 res.send("updated data successfully");
 })
-
+//Tar bort vänner från min lista och databas
 app.delete('/delete/:id', async(req, res) => {
     const id = req.params.id
     await FriendModel.findByIdAndRemove(id).exec()
     res.send("raderad");
 })
-
+//lyssnar på port 3001 men kan även använda den live via process.env.Port med mitt projekt.
 app.listen(process.env.PORT || 3001,() => {console.log('du är ansluten'); 
 });
